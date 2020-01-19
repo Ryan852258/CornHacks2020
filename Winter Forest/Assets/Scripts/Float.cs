b0fec4995f9d15e9df9028e086eb8968014ca50b
Chase Michael Pearson <cpearson>
2020-01-18 20:18:12 -0600

    Float script

---
+++ Winter Forest/Assets/Scripts/Float.cs
@@ -0,0 +1,42 @@
+﻿// Floater v0.0.2
+// by Donovan Keith
+//
+// [MIT License](https://opensource.org/licenses/MIT)
+
+using UnityEngine;
+using System.Collections;
+
+// Makes objects float up & down while gently spinning.
+public class Float : MonoBehaviour
+{
+    // User Inputs
+    public float xRotation = 15.0f;
+    public float yRotation = 15.0f;
+    public float zRotation = 15.0f;
+    public float amplitude = 0.5f;
+    public float frequency = 1f;
+
+    // Position Storage Variables
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
+    // Use this for initialization
+    void Start()
+    {
+        // Store the starting position & rotation of the object
+        posOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Spin object around Y-Axis
+        transform.Rotate(new Vector3(Time.deltaTime * xRotation, Time.deltaTime * yRotation, Time.deltaTime * zRotation), Space.World);
+
+        // Float up/down with a Sin()
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
+    }
+}
